049f9e25e41c87ed5b7858dac63d679afdc02cc1
muhammad umair <umairkhalid1314@gmail.com>
2024-12-13 04:04:46 +0500

    login api added

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,9 +1,19 @@
-# from django.contrib import admin
-# from .models import User
-# # Register your models here.
+from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# class CustomUserAdmin(admin.ModelAdmin):
-#     model=User
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    model = User
+    list_display = ['username', 'role', 'is_active', 'is_staff']
+    search_fields = ['username', 'role']
+    list_filter = ['role', 'is_active', 'is_staff']
 
+    def save_model(self, request, obj, form, change):
+        """
+        Override save_model to hash the password if it's being changed.
+        """
+        if form.cleaned_data.get("password") and not obj.password.startswith("pbkdf2_"):
+            obj.set_password(form.cleaned_data["password"])
+        super().save_model(request, obj, form, change)
 
-# admin.register(User,CustomUserAdmin)
